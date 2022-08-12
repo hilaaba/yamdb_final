233649d59af05431039620ef115ae44175270d3c
Вячеслав Иванов <hilaaba@yandex.ru>
2022-08-12 12:00:30 +0300

    some fix

--- api_yamdb/run.sh
+++ api_yamdb/run.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-python manage.py makemigrations
-python manage.py migrate
-python manage.py loaddata fixtures.json
-python manage.py collectstatic --no-input
 gunicorn api_yamdb.wsgi:application --bind 0:8000
